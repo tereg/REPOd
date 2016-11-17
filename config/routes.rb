ee7e719f03aa1afb97556e9745c5d6772847a7a3
tereg <teregalla@gmail.com>
2016-11-16 18:43:32 -0800

    Update routes for signup, login, and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup' => 'users#new'
+  resources :users
+
+  get 'login' => 'sessions#new'
+  resources :sessions
+
+  post 'login' => 'sessions#create'
+
+  delete 'logout' => 'sessions#destroy'
 end
