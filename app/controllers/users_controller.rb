b6f1947f60a753db83fc3df8081e976cfa62b69c
Tony Nguyen <tntuyen@ucdavis.edu>
2016-11-16 17:08:47 -0800

    adds Posts controller/routes and views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @posts = @user.posts
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   get 'sessions/destroy'
 
   root 'users#index'
-  resources :users
+  resources :users, :posts
 
-  get '/users/login' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
