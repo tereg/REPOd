3d6b9f74b174004784bf660432b46deca6ac1af0
Tony Nguyen <tntuyen@ucdavis.edu>
2016-11-16 16:29:23 -0800

    adds users and sessions controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def login
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/destroy'
+
+  root 'users#index'
+  resources :users
+
+  get '/users/login' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
