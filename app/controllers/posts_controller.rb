10e4536418997fba533f772a24f3bc19603a7429
Frank Sommers <email": null>
2016-11-17 10:42:25 -0800

    adds new post form

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,22 +1,29 @@
 class PostsController < ApplicationController
-  
+
   def index
     @posts = Post.all
   end
-  
-  def new
 
-  end   
-  
+  def new
+    @user = User.find(current_user.id)
+    @post = @user.posts.new
+  end
+
   def create
-  
+    @user = User.find(current_user.id)
+    @post = @user.posts.new(post_params)
+    if @post.save
+      redirect_to root_path
+    else
+     render "new"
+    end
   end
 
   def show
     @post = Post.find(params[:id])
   end
 
-  
+
   def edit
 
   end
@@ -29,4 +36,10 @@ class PostsController < ApplicationController
   def destroy
 
   end
+
+  private
+  def post_params
+    params.require(:post).permit(:title, :description, :media_type, :url, :user_id, :verified)
+  end
+
 end
