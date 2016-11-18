dbfd953aa1de95d2bb84d9147f603567bef7126a
Robbie Santos <robbiealixsantos@gmail.com>
2016-11-18 12:33:18 -0800

    Add partially completed admin feature

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -8,7 +8,7 @@ class AdminsController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
-      redirect_to edit_admin(@user)
+      redirect_to admin_path(@user)
     else
       redirect_to signup_path
     end
